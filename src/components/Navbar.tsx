7955a183b6aa792e11f66fd510ef7667ae003dda
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-08-11 19:52:41 +0200

    àdd: animations 1.50

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 const Navbar = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
-
-  const isAdmin = user?.email === process.env.ADMIN_EMAIL
+  const adminEmails: Array<string> = process.env.ADMIN_EMAIL?.split(",") || []
+  const isAdmin: boolean = adminEmails.includes(user?.email || "")
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
